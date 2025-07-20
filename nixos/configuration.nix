a241b3861c8abf4ba22b5771b048327cadf10ccf
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-07-20 14:50:49 +0400

    lutis partitionmanager partclone

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -99,6 +99,7 @@ in
     isNormalUser = true;
     description = "Game";
     packages = with pkgs; [
+      lutris
     ];
   };
 
@@ -114,6 +115,7 @@ in
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    kdePackages.partitionmanager
     vscode
     pciutils
     nekoray
@@ -123,6 +125,7 @@ in
     mpv
     bitwarden-desktop
     qdirstat
+    partclone
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
