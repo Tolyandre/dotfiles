3ab9cb2412074a05adcae1ad7638a52758f2f2dd
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-09-27 14:37:07 +0400

    Fix mysql backup

--- nixos/mysql.nix
+++ nixos/mysql.nix
@@ -3,6 +3,7 @@
   # mysql (mariadb) is a dependency for seafile
   services.mysql.settings = {
     mysqld = {
+      # override to bind to localhost only
       bind_address = "127.0.0.1";
     };
   };
@@ -10,5 +11,10 @@
   services.mysqlBackup = {
     enable = true;
     location = "/mnt/seagate/Backup/mysql_mariadb";
+    databases = [
+      "ccnet_db"
+      "seafile_db"
+      "seahub_db"
+    ];
   };
 }
