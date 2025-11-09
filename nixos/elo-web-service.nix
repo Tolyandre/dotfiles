dcfce21b08f9657bbf40861479dd15ec7a27e62f
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-11-10 00:27:05 +0400

    Update elo web service

--- nixos/elo-web-service.nix
+++ nixos/elo-web-service.nix
@@ -6,8 +6,15 @@
 
   services.elo-web-service = {
     enable = true;
+    secrets-env-file = "/my-secrets/elo-web-service/secrets.env";
     google-service-account-key = "/my-secrets/elo-web-service/elo-project-466111-adeffde84816.json";
-    doc-id = "1bf6bmd63dvO9xjtnoTGmkcWJJE0NetQRjKkgcQvovQQ";
-    address = "localhost:42981";
+    config = {
+      doc_id = "1bf6bmd63dvO9xjtnoTGmkcWJJE0NetQRjKkgcQvovQQ";
+      address = "localhost:42981";
+      oauth2_auth_uri= https://accounts.google.com/o/oauth2/auth;
+      oauth2_redirect_uri= https://toly.is-cool.dev/elo-web-service/auth/oauth2-callback;
+      oauth2_token_uri= https://oauth2.googleapis.com/token;
+      frontend_uri= https://tolyandre.github.io/elo;
+    };
   };
 }
