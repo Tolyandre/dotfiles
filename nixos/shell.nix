03532a9b1df9c94f2d8841348802d3bc14953dfd
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-11-19 22:08:59 +0400

    fix my-update

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -2,8 +2,8 @@
 {
   environment.systemPackages = with pkgs; [
     (writeShellScriptBin "my-update" ''
-      HTTPS_PROXY="socks5://127.0.0.1:20170"
-      HTTP_PROXY="socks5://127.0.0.1:20170"
+      export HTTPS_PROXY="socks5://127.0.0.1:20170"
+      export HTTP_PROXY="socks5://127.0.0.1:20170"
 
       nixos-rebuild switch --upgrade
       
