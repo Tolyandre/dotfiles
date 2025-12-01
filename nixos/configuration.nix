23df77022d66c4ee2894fc3622b9fd5ba6b84f8f
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-12-01 20:56:03 +0400

    Pipewire systemwide

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,6 +21,7 @@ in
     ./ocis.nix
     ./immich.nix
     ./open-webui.nix
+    ./pipewire.nix
     ./podman.nix
     ./postgresql.nix
     ./seafile.nix
@@ -94,22 +95,6 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
@@ -132,6 +117,7 @@ in
     extraGroups = [
       "networkmanager"
       "wheel"
+      "pipewire"
     ];
     packages = with pkgs; [
       kdePackages.kate
@@ -142,6 +128,9 @@ in
   users.users.game = {
     isNormalUser = true;
     description = "Game";
+    extraGroups = [
+      "pipewire"
+    ];
     packages = with pkgs; [
       lutris
     ];
