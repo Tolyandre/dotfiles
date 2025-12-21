c68eed25913dc01f2a063bf074d0b2eba4903022
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-12-21 23:26:05 +0400

    guacamole try (does not work)

--- nixos/guacamole.nix
+++ nixos/guacamole.nix
@@ -1,22 +1,22 @@
 { config, pkgs, ... }:
 
-let
-  # старый nixpkgs, где freerdp2 ещё есть
-  oldpkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz";
-    sha256 = "1f5d2g1p6nfwycpmrnnmc2xmcszp804adp16knjvdkj8nz36y1fg";
-  }) { };
-in
+# let
+#   # старый nixpkgs, где freerdp2 ещё есть
+#   oldpkgs = import (fetchTarball {
+#     url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz";
+#     sha256 = "1f5d2g1p6nfwycpmrnnmc2xmcszp804adp16knjvdkj8nz36y1fg";
+#   }) { };
+# in
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      guacamole-server = super.guacamole-server.overrideAttrs (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.pkg-config ];
-        buildInputs = (old.buildInputs or [ ]) ++ [ oldpkgs.freerdp ];
-        configureFlags = (old.configureFlags or [ ]) ++ [ "--with-freerdp" ];
-      });
-    })
-  ];
+  #   nixpkgs.overlays = [
+  #     (self: super: {
+  #       guacamole-server = super.guacamole-server.overrideAttrs (old: {
+  #         nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ super.pkg-config ];
+  #         buildInputs = (old.buildInputs or [ ]) ++ [ oldpkgs.freerdp ];
+  #         configureFlags = (old.configureFlags or [ ]) ++ [ "--with-freerdp" ];
+  #       });
+  #     })
+  #   ];
 
   services.guacamole-server = {
     enable = true;
@@ -39,5 +39,7 @@ in
   };
 
   services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "startplasma-x11"; # does not work
+  #services.xrdp.defaultWindowManager = "startplasma-x11"; # does not work
+  #services.xserver.enable = true;
+  #services.xserver.displayManager.sddm.enable = true;
 }
