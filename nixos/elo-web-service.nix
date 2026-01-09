93f2bec0a87beef59b4e64167e5068ed1a3ef749
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2026-01-09 15:21:07 +0400

    Elo-web-service add postgres

--- nixos/elo-web-service.nix
+++ nixos/elo-web-service.nix
@@ -11,11 +11,14 @@
     config = {
       doc_id = "1bf6bmd63dvO9xjtnoTGmkcWJJE0NetQRjKkgcQvovQQ";
       address = "localhost:42981";
-      oauth2_auth_uri= https://accounts.google.com/o/oauth2/auth;
-      oauth2_redirect_uri= https://tolyandre.github.io/elo/oauth2-callback;
-      oauth2_token_uri= https://oauth2.googleapis.com/token;
-      frontend_uri= https://tolyandre.github.io/elo;
-      cookie_ttl_seconds= 2592000; # 3600*24*30
+      oauth2_auth_uri = "https://accounts.google.com/o/oauth2/auth";
+      oauth2_redirect_uri = "https://tolyandre.github.io/elo/oauth2-callback";
+      oauth2_token_uri = "https://oauth2.googleapis.com/token";
+      frontend_uri = "https://tolyandre.github.io/elo";
+      cookie_ttl_seconds = 2592000; # 3600*24*30
+      postgres = {
+        enableLocalDatabase = true;
+      };
     };
   };
 }
