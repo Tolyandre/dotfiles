ec2042cd5d04a09c4a9babf292d83397a35f2a09
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2026-04-03 01:28:55 +0400

    feat: my-update-elo

--- nixos-desktop/shell.nix
+++ nixos-desktop/shell.nix
@@ -1,6 +1,20 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "my-update-elo" ''
+      set -e
+
+      export HTTPS_PROXY="socks5://127.0.0.1:20170"
+      export HTTP_PROXY="socks5://127.0.0.1:20170"
+
+      nix flake update elo --flake /dotfiles-repo
+
+      nixos-rebuild switch --flake /dotfiles-repo#nixos-desktop
+
+      echo ""
+      nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
+    '')
+
     (writeShellScriptBin "my-update" ''
       set -e
 
