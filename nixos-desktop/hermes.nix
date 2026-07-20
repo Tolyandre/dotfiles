97545ca444a56356f75d18b9d89170471c4e5ee7
Anatolii Buranov <2414704+Tolyandre@users.noreply.github.com>
2026-07-20 19:17:15 +0400

    chore: disable hermes

--- nixos-desktop/hermes.nix
+++ nixos-desktop/hermes.nix
@@ -1,12 +1,14 @@
 { hermes-agent, pkgs, ... }: {
-  # https://hermes-agent.nousresearch.com/docs/getting-started/nix-setup?utm_source=chatgpt.com#minimal-configuration
-  services.hermes-agent = {
-    enable = true;
-    addToSystemPackages = true;
-  };
+  # # https://hermes-agent.nousresearch.com/docs/getting-started/nix-setup#minimal-configuration
+  # services.hermes-agent = {
+  #   enable = false;
+  #   addToSystemPackages = true;
+  # };
 
-  # run hermes-desktop
-  environment.systemPackages = [
-    hermes-agent.packages.${pkgs.system}.desktop
-  ];
+  # # hermes-desktop
+  # # you can also run without install: 
+  # # nix run github:NousResearch/hermes-agent#desktop
+  # environment.systemPackages = [
+  #   hermes-agent.packages.${pkgs.system}.desktop
+  # ];
 }
