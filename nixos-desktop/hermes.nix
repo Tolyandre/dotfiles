b287beed994cb4b079342b679e02bc3dd416a1b0
Anatolii Buranov <2414704+Tolyandre@users.noreply.github.com>
2026-08-09 20:17:11 +0400

    chore: add hermes back

--- nixos-desktop/hermes.nix
+++ nixos-desktop/hermes.nix
@@ -1,14 +1,14 @@
 { hermes-agent, pkgs, ... }: {
-  # # https://hermes-agent.nousresearch.com/docs/getting-started/nix-setup#minimal-configuration
-  # services.hermes-agent = {
-  #   enable = false;
-  #   addToSystemPackages = true;
-  # };
+  # https://hermes-agent.nousresearch.com/docs/getting-started/nix-setup#minimal-configuration
+  services.hermes-agent = {
+    enable = false;
+    addToSystemPackages = true;
+  };
 
-  # # hermes-desktop
-  # # you can also run without install: 
-  # # nix run github:NousResearch/hermes-agent#desktop
-  # environment.systemPackages = [
-  #   hermes-agent.packages.${pkgs.system}.desktop
-  # ];
+  # hermes-desktop
+  # you can also run without install: 
+  # nix run github:NousResearch/hermes-agent#desktop
+  environment.systemPackages = [
+    hermes-agent.packages.${pkgs.system}.desktop
+  ];
 }
