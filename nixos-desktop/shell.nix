fc90b6491058d35888fd0ebbd2b20fb3a2b72c6a
Anatolii Buranov <2414704+Tolyandre@users.noreply.github.com>
2026-08-27 10:17:59 +0400

    update

--- nixos-desktop/shell.nix
+++ nixos-desktop/shell.nix
@@ -32,6 +32,19 @@
       nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
     '')
 
+    (writeShellScriptBin "my-update-nh" ''
+      set -e
+
+      export https_proxy="http://127.0.0.1:2080"
+      export http_proxy="http://127.0.0.1:2080"
+      export no_proxy="localhost,127.0.0.1"
+
+      nix-channel --update
+      nix flake update --flake /dotfiles-repo
+
+      nh os switch /dotfiles-repo#nixos-desktop
+    '')
+
     # Reset the AMD GPU after the system wakes from sleep (6700XT reset bug).
     # Must be run as root: it writes to /sys, arms an rtcwake alarm, and suspends.
     # Based on https://forum.level1techs.com/t/6700xt-reset-bug/181814/19
